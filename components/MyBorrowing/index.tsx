f667ae6cf30ead9ce3f754cda70953b680271031
Jason Yang <jasonyang@Jasons-MacBook-Pro.local>
2022-11-06 01:34:53 -0700

    create boiler function for all buttons

--- components/MyBorrowing/index.tsx
+++ components/MyBorrowing/index.tsx
@@ -4,51 +4,32 @@ import BorrowingModal, { Loan } from 'components/BorrowingModal';
 
 const loans:Loan[] = [
   {
-    asset: 'MY BORROW',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    borrowerAddress: '0xborrower address1',
+    collateralTokenAddress: '0xcollateral token address1',
+    collateralToken: 'cbETH',
+    collateralAmount: 1.25,
+    amtUSDCToBorrow: 1234,
+    loanDuration: "4 Weeks",
+    status: "Pending",
   },
   {
-    asset: 'VLCVX',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    borrowerAddress: '0xborrower address2',
+    collateralTokenAddress: '0xcollateral token address2',
+    collateralToken: 'cbETH',
+    collateralAmount: 1.5,
+    amtUSDCToBorrow: 124,
+    loanDuration: "8 Weeks",
+    status: "Pending",
   },
   {
-    asset: 'VLCVX',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    borrowerAddress: '0xborrower address3',
+    collateralTokenAddress: '0xcollateral token address3',
+    collateralToken: 'cbETH',
+    collateralAmount: 2.25,
+    amtUSDCToBorrow: 345,
+    loanDuration: "16 Weeks",
+    status: "Pending",
   },
-  {
-    asset: 'VLCVX',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    asset: 'VLCVX',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  // More people...
 ]
 
 export default function BorrowingPage() {
@@ -64,56 +45,60 @@ export default function BorrowingPage() {
               <table className="min-w-full divide-y divide-gray-300">
                 <thead className="bg-gray-50">
                   <tr>
-                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                      Token
+                  <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                      Collateral Token
                     </th>
                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                       Collateral Amount
                     </th>
                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Date
+                      Amount of USDC to Borrow
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Loan Duration
                     </th>
                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                       Status
                     </th>
-                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                      <span className="sr-only">Repay</span>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      View
                     </th>
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {loans.map((person) => (
-                    <tr key={person.email}>
+                  {loans.map((loan) => (
+                    <tr key={loan.borrowerAddress + loan.collateralTokenAddress}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                        <div className="flex items-center">
-                          <div className="h-10 w-10 flex-shrink-0">
+                        {/* <div className="flex items-center"> */}
+                          {/* <div className="h-10 w-10 flex-shrink-0">
                             <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-                          </div>
+                          </div> */}
                           <div className="ml-4">
-                            <div className="font-medium text-gray-900">{person.asset}</div>
-                            <div className="text-gray-500">{person.email}</div>
+                            <div className="font-medium text-gray-900">{loan.collateralToken}</div>
                           </div>
-                        </div>
+                        {/* </div> */}
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        <div className="text-gray-900">{person.title}</div>
-                        <div className="text-gray-500">{person.department}</div>
+                        <div className="text-gray-900">{loan.collateralAmount}</div>
+                        {/* <div className="text-gray-500">{person.department}</div> */}
                       </td>
+                      
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{loan.amtUSDCToBorrow}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{loan.loanDuration}</td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                          Active
+                        <span className="inline-flex rounded-full bg-blue-100 px-2 text-xs font-semibold leading-5 text-blue-800">
+                          Pending
                         </span>
                       </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.role}</td>
                       <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                      <button
-                          className="text-indigo-600 hover:text-indigo-900"
+                        <button 
                           type="button"
+                          className="text-indigo-600 hover:text-indigo-900"
                           onClick={() => {
                             setIsViewingLoan(!isViewingLoan);
-                            setCurrentlySelectedLoan(person);
+                            setCurrentlySelectedLoan(loan);
                           }}>
-                          Repay<span className="sr-only">test button value</span>
+                          View<span className="sr-only">test button value</span>
                         </button>
                       </td>
                     </tr>
--- components/NewLoan/index.tsx
+++ components/NewLoan/index.tsx
@@ -1,45 +1,6 @@
-import { Fragment, useState } from 'react'
+import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
-import { LinkIcon, PlusIcon, QuestionMarkCircleIcon } from '@heroicons/react/20/solid'
-
-const team = [
-  {
-    name: 'Tom Cook',
-    email: 'tom.cook@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Whitney Francis',
-    email: 'whitney.francis@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Leonard Krasner',
-    email: 'leonard.krasner@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Floyd Miles',
-    email: 'floy.dmiles@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Emily Selman',
-    email: 'emily.selman@example.com',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-]
 
 export default function NewLoan({setIsOpen}: {setIsOpen:Function}) {
 
